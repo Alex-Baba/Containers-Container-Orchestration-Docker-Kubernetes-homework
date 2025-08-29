c74db1d361619498298a69246684bfa5f741ccf9
Alexandru Baba <Alexandru.Baba@endava.com>
2025-08-29 11:35:02 +0300

    added gitignore and healthcheck

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 # Run the app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
